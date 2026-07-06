c545fc9e2253d285f4ffe0fd5120d81dc6d7fce4
mattharg <mattharg@gmail.com>
2026-07-06 12:19:50 +1000

    Second design change

---
+++ scripts/makeNewTag.sh
@@ -0,0 +1,7 @@
+cd /Users/mattharg/dev/jekyll-tailwind-platform
+
+git tag v0.1.1
+git push github v0.1.1
+
+
+
---
+++ scripts/retag.sh
@@ -0,0 +1,4 @@
+git tag -d v0.1.0
+git push github :refs/tags/v0.1.0
+git tag v0.1.0
+git push github v0.1.0
